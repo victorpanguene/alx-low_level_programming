e923c870d0671ec81f56ef8f95f1d754074c9d1d
Victor Panguene <svpanguene@gmail.com>
2022-12-08 14:19:02 -0800

    Task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - ENTRY
- * Return: Always 0
- */ 
-
+ *main - ENTRY
+ *Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
