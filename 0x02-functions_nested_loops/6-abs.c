7bd6ff2ab467093e969b49064558a4586aa63531
Victor Panguene <svpanguene@gmail.com>
2022-12-14 10:52:53 -0800

    task 6 fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /*
- * _abs:prints the absolute value of an integer.
+ * @_abs:prints the absolute value of an integer.
  * @a:the integer to compute its absolute value.
- * absvalue is the abs value.
+ * @absvalue is the abs value.
  * Return:returns the abs value.
  */
 int _abs(int a)
