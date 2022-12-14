3845dcb98148b66675059e36b016786f3a3d511c
Victor Panguene <svpanguene@gmail.com>
2022-12-14 12:36:41 -0800

    Task 8 fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * jack_bauer-prints all minutes in a day from 00:00
+ * jack_bauer - prints all minutes in a day from 00:00
  * Return:0 if succeesful.
- */
-
+*/
 void jack_bauer(void)
 {
 	int i, j, k, m;
