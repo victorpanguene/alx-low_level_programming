be0bf5beb0349e49edeeb7228857407bd8333b89
Victor Panguene <svpanguene@gmail.com>
2022-12-14 13:44:15 -0800

    Task 8 fix 3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,10 @@
 void jack_bauer(void)
 {
 	int i, j, k, m;
+
 	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
@@ -14,7 +17,7 @@ void jack_bauer(void)
 				{
 					if (i == 2 && j > 3)
 					{break;
-					}
+					 }
 					_putchar(i + '0');
 					_putchar(j + '0');
 					_putchar(':');
@@ -24,4 +27,5 @@ void jack_bauer(void)
 				}
 			}
 		}
+	}
 }
