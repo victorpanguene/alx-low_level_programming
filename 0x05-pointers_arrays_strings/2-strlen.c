37fcfae05a64301d4a8c6f67d848eb9c95be1a68
Victor Panguene <svpanguene@gmail.com>
2022-12-19 09:39:12 -0800

    task 2 correct

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,13 @@
 
 int _strlen(char *s)
 {
-	int p = 0;
-	/incremeant up to when the last character is NULL,\0/
-	while (*(s + p) != 0)
+	int len = 0;
+
+	while (*s  != '\0')
 	{
-		p++;
+		len++;
+		s++;
 	}
-	return (p);
+
+	return (len);
 }
